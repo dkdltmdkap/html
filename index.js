a0d1b39deb67a0e840fd987b70f18b661989f4df
kde <sue050533@naver.com>
2025-06-05 17:56:48 +0900

    메인 배경 추가 & 컬러박스 팝업 구성 및 웹페이지 슬라이드 구성, 슬라이드 화살표 색상변경 & 인덱스 팝업 처리 & 슬라이드 구역작업 및 마지막 세션 스크롤 작업

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 $('#fullpage').fullpage({
 	autoScrolling:true,
-	scrollHorizontally: true,
+	// scrollHorizontally: true,
     navigation: true,
-    navigationPositon: 'right'
+    navigationPositon: 'right',
+    anchors: ['1', '2', '3', '4'],
+    menu: '.menu',
+    afterLoad: function(origin, destination, direction, trigger) {
+        console.log(destination.index);
+        if (destination.index == 3) {
+            $.fn.fullpage.setAutoScrolling(false);
+        } else {
+            $.fn.fullpage.setAutoScrolling(true);
+        }
+    }
+});
+
+$('.d1').colorbox({rel: 'gal'});
+$('.d1pop').colorbox({iframe:true, innerWidth: 800, innerHeight: 600});
+
+$('.win').on('click', ()=>{
+    window.open('./popup.html', 'popup', 'width=600, height=400');
 });
