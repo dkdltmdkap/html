4a57c0e5491eb687b3952a8a2206e81ebd392372
kde <sue050533@naver.com>
2025-06-10 15:20:56 +0900

    nav bar 수정 & 반응형 리뉴얼 틀 잡기

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 $('#fullpage').fullpage({
-	autoScrolling:true,
-	// scrollHorizontally: true,
+    autoScrolling: true,
+    scrollHorizontally: true,
     navigation: true,
     navigationPositon: 'right',
     anchors: ['1', '2', '3', '4'],
     menu: '.menu',
-    afterLoad: function(origin, destination, direction, trigger) {
+    afterLoad: function (origin, destination, direction, trigger) {
         console.log(destination.index);
-        if (destination.index == 3) {
-            $.fn.fullpage.setAutoScrolling(false);
-        } else {
+        if (destination.index == 0) {
             $.fn.fullpage.setAutoScrolling(true);
+            document.querySelector('.menu').style.display = 'none';
+        } else if (destination.index <= 2) {
+            $.fn.fullpage.setAutoScrolling(true);
+            document.querySelector('.menu').style.display = 'block';
+        } else if (destination.index == 3) {
+            $.fn.fullpage.setAutoScrolling(false);
+            document.querySelector('.menu').style.display = 'block';
         }
     }
 });
 
-$('.d1').colorbox({rel: 'gal'});
-$('.d1pop').colorbox({iframe:true, innerWidth: 800, innerHeight: 600});
+$('.d1').colorbox({ rel: 'gal' });
+$('.d1pop').colorbox({ iframe: true, innerWidth: 800, innerHeight: 600 });
 
-$('.win').on('click', ()=>{
+$('.win').on('click', () => {
     window.open('./popup.html', 'popup', 'width=600, height=400');
 });
