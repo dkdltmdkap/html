d4c999bd4a3e00a99497f56127635c948d78efda
kde <sue050533@naver.com>
2025-08-09 17:22:41 +0900

    main 페이지

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // 페이지 조정
 $('#fullpage').fullpage({
-<<<<<<< HEAD
     anchors: ['HOME', 'ABOUTME', 'SKILL', 'WORK', 'CONTACT'],
     menu: 'nav',
     autoScrolling: true,
@@ -72,32 +71,12 @@ $('#fullpage').fullpage({
             animateCircle('skill-ps', 50, '#31a8ff');
             animateCircle('skill-ai', 50, '#ff9a00');
             animateCircle('skill-figma', 70, '#a259ff');
-=======
-    autoScrolling: true,
-    scrollHorizontally: true,
-    navigation: true,
-    navigationPositon: 'right',
-    anchors: ['1', '2', '3', '4'],
-    menu: '.menu',
-    afterLoad: function (origin, destination, direction, trigger) {
-        // console.log(destination.index);
-        if (destination.index == 0) {
-            $.fn.fullpage.setAutoScrolling(true);
-            document.querySelector('.menu').style.display = 'none';
-        } else if (destination.index <= 2) {
-            $.fn.fullpage.setAutoScrolling(true);
-            document.querySelector('.menu').style.display = 'block';
-        } else if (destination.index == 3) {
-            $.fn.fullpage.setAutoScrolling(false);
-            document.querySelector('.menu').style.display = 'block';
->>>>>>> 9f128699fb36964ac6eebce5fea1f5d500630d6f
         }
     }
 });
 
 // mini-project를 popup창 띄우기
 // $('.d1').colorbox({ rel: 'gal' });
-<<<<<<< HEAD
 // $('.d1').colorbox({ iframe: true, innerWidth: 1240, innerHeight: 422 });
 // $('.d2').colorbox({ iframe: true, innerWidth: 1840, innerHeight: 800 });
 // $('.d3').colorbox({ iframe: true, innerWidth: 1075, innerHeight: 881 });
@@ -165,35 +144,25 @@ document.addEventListener('DOMContentLoaded', () => {
     animateCircle('skill-ai', 50, '#ff9a00');         // Illustrator
     animateCircle('skill-figma', 70, '#a259ff');      // Figma
 });
-=======
-$('.d1').colorbox({ iframe: true, innerWidth: 1240, innerHeight: 422 });
-$('.d2').colorbox({ iframe: true, innerWidth: 1840, innerHeight: 800 });
-$('.d3').colorbox({ iframe: true, innerWidth: 1075, innerHeight: 881 });
-$('.d4').colorbox({ iframe: true, innerWidth: 548, innerHeight: 573 });
-$('.d5').colorbox({ iframe: true, innerWidth: 1062, innerHeight: 796 });
-$('.d6').colorbox({ iframe: true, innerWidth: 582, innerHeight: 834 });
-$('.d7').colorbox({ iframe: true, innerWidth: 1393, innerHeight: 407 });
 
-// skills 그래프로 출력
-let ctx = document.querySelector('#skills').getContext('2d');
-ctx.strokeStyle = 'rgba(44, 43, 43, 0.8)';
 
-let dis = 0;
-for (let i = 0; i < 7; i++) {
-    ctx.beginPath();
-    ctx.moveTo(dis, 570);
-    ctx.lineTo(dis, 35);
-    ctx.stroke();
-    dis += 115;
-}
-ctx.closePath();
+// 포트폴리오 페이지 넘기기
+const miniProjectUl = document.querySelector('.miniProject_list'); // ✅ 슬라이드 대상
+const prevBtn = document.querySelector('.prevBtn');
+const nextBtn = document.querySelector('.nextBtn');
 
-ctx.fillStyle = '372948';
-ctx.fillRect(0, 60, 115, 30);
-ctx.fillRect(0, 135, 115, 30);
-ctx.fillRect(0, 210, 233, 30);
-ctx.fillRect(0, 290, 300, 30);
-ctx.fillRect(0, 365, 280, 30);
-ctx.fillRect(0, 445, 250, 30);
-ctx.fillRect(0, 525, 10, 30);
->>>>>>> 9f128699fb36964ac6eebce5fea1f5d500630d6f
+let currentIndex = 0;
+
+prevBtn.addEventListener('click', () => {
+    if (currentIndex > 0) {
+        currentIndex--;
+        miniProjectUl.style.transform = `translateX(-${100 * currentIndex}vw)`;
+    }
+});
+
+nextBtn.addEventListener('click', () => {
+    if (currentIndex < 1) {
+        currentIndex++;
+        miniProjectUl.style.transform = `translateX(-${100 * currentIndex}vw)`;
+    }
+});
